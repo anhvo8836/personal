b621e318de1895fd51fcb8207cf76961a0e75c8b
anhvo8836 <109463460+anhvo8836@users.noreply.github.com>
2022-07-19 16:29:27 -0400

    Update config.fish
    updating fish config

--- configs/config.fish
+++ configs/config.fish
@@ -1,14 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-## Maintenance Alias
+# Maintenance Alias
 alias maintain="yay -Syu && yay -Scc"
 alias update="yay -Syu"
 alias clean-cache="yay -Scc"
 alias clean-orphans="yay -Qttdq | yay -Rns -"
 alias packages="yay -Qq"
 
-#Operations Alias
+# Operations Alias
 alias ls="ls -lah --color=always"
 alias install="yay -S"
 alias remove="yay -Rns"
@@ -18,7 +18,11 @@ alias search-repo="yay -Si"
 alias edit="sudo nano"
 alias delete="sudo rm -rf"
 alias clear="clear && neofetch"
-alias phone="adb connect 192.168.2.34:5555 && scrcpy"
+alias restart="source ~/.config/fish/config.fish"
+
+# Git
+alias update-alibi="cd ~/Repositories/alibi;git pull;cd"
+alias update-personal="cd ~/Repositories/personal;git pull;cd"
 
 neofetch
 
