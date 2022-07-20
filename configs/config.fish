c23c03652fea84ad7c8b29353b7f8e9d31aee866
anhvo8836 <109463460+anhvo8836@users.noreply.github.com>
2022-07-20 07:53:30 -0400

    Update config.fish
    updating fish config

--- configs/config.fish
+++ configs/config.fish
@@ -1,28 +1,46 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-# Maintenance Alias
-alias maintain="yay -Syu && yay -Scc"
-alias update="yay -Syu"
-alias clean-cache="yay -Scc"
-alias clean-orphans="yay -Qttdq | yay -Rns -"
-alias packages="yay -Qq"
+# Remove greeting message
 
-# Operations Alias
-alias ls="ls -lah --color=always"
-alias install="yay -S"
-alias remove="yay -Rns"
-alias force-remove="yay -Rdd"
-alias search-local="yay -Qi"
-alias search-repo="yay -Si"
-alias edit="sudo nano"
-alias delete="sudo rm -rf"
-alias clear="clear && neofetch"
-alias restart="source ~/.config/fish/config.fish"
+set fish_greeting
 
-# Git
-alias update-alibi="cd ~/Repositories/alibi;git pull;cd"
-alias update-personal="cd ~/Repositories/personal;git pull;cd"
+# Powerline
+# powerline and powerline-fonts must be installed for this function to work properly
+
+#set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+#source /usr/share/powerline/bindings/fish/powerline-setup.fish
+#powerline-setup
+
+# Aliases
+
+    # Maintenance Alias
+
+    alias maintain="yay -Syu && yay -Scc"
+    alias update="yay -Syu"
+    alias clean-cache="yay -Scc"
+    alias clean-orphans="yay -Qttdq | yay -Rns -"
+    alias packages="yay -Qq"
+
+    # Operations Alias
+
+    alias ls="ls -lah --color=always"
+    alias install="yay -S"
+    alias remove="yay -Rns"
+    alias force-remove="yay -Rdd"
+    alias search-local="yay -Qi"
+    alias search-repo="yay -Si"
+    alias edit="sudo nano"
+    alias delete="sudo rm -rf"
+    alias clear="clear && neofetch"
+    alias restart="source ~/.config/fish/config.fish"
+
+    # Git
+
+    alias clone="git clone"
+    alias pull="git pull"
+    alias update-alibi="cd ~/Repositories/alibi;git pull;cd"
+    alias update-personal="cd ~/Repositories/personal;git pull;cd"
 
 neofetch
 
